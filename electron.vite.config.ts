7c70c5d70026c8f2469cb52f79d361dbb3ff224a
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-14 18:23:13 +0530

    update electron.vite.config.ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    build: {
+      bytecode: true // Protect main process (your agent logic)
+    }
+  },
+  preload: {
+    build: {
+      bytecode: true // Protect main process (your agent logic)
+    }
+  },
   renderer: {
     publicDir: resolve('src/renderer/src/public'),
     resolve: {
